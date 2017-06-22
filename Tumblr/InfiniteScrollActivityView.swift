7c82a75e192e436e04ad976533af62fa8b4dc386
Dominique Adapon <dadapon@fb.com>
2017-06-22 16:59:16 -0700

    Finished infinite scrolling

---
+++ Tumblr/InfiniteScrollActivityView.swift
@@ -0,0 +1,21 @@
+//
+//  InfiniteScrollActivityView.swift
+//  Tumblr
+//
+//  Created by Dominique Adapon on 6/22/17.
+//  Copyright © 2017 Dominique Adapon. All rights reserved.
+//
+
+import UIKit
+
+class InfiniteScrollActivityView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
